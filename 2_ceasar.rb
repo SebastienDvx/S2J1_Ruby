159b34b8ab4b88a6db9b4c65e3c8893be4139128
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-15 18:25:13 +0100

    Exo CEASAR : DONE ! + gestion espaces

--- 2_ceasar.rb
+++ 2_ceasar.rb
@@ -57,24 +57,28 @@
 #VERSION COURTE 1
 # string = "abcd";
 # # n = 3;
-# def chiffre_de_cesar(string, n)
-# string.each_byte do |valeur_ascii_de_la_lettre|
-# valeur_decalee = valeur_ascii_de_la_lettre + n;
-# print valeur_decalee.chr
-# # print ascii_vers_caractere(valeur_decalee);
-# end
-# end
-# chiffre_de_cesar("hello", 3)
-# puts
+def chiffre_de_cesar(string, n)
+string.each_byte do |valeur_ascii_de_la_lettre|
+if valeur_ascii_de_la_lettre == 32
+  valeur_decalee = valeur_ascii_de_la_lettre
+else
+  valeur_decalee = valeur_ascii_de_la_lettre + n;
+end
+print valeur_decalee.chr
+# print ascii_vers_caractere(valeur_decalee);
+end
+end
+chiffre_de_cesar("the hacking project", 3)
+puts
 
 
 #VERSION COURTE 2 MEGA COURTE
-def chiffre_de_cesar(string, n)
-string.each_byte { |valeur_ascii_de_la_lettre| valeur_decalee = valeur_ascii_de_la_lettre + n;
-print valeur_decalee.chr}
-# print ascii_vers_caractere(valeur_decalee);
-end
-chiffre_de_cesar("abcd", 3)
-puts
+# def chiffre_de_cesar(string, n)
+# string.each_byte { |valeur_ascii_de_la_lettre| valeur_decalee = valeur_ascii_de_la_lettre + n;
+# print valeur_decalee.chr}
+# # print ascii_vers_caractere(valeur_decalee);
+# end
+# chiffre_de_cesar("abcd", 3)
+# puts
 
 #AJOUTER UNE CONDITION POUR GERER LES ESPACES !!
